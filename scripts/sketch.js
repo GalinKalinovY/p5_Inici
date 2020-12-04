c200e2d5980fb8083e6de120cd50e24b0f134030
GalinKalinovY <gyordanov@almata.cat>
2020-12-04 19:13:49 +0100

    Hem afegit un pacman que el podem moure amb el ratoli.

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,21 +1,26 @@
-let y = 100;
 
+let pacman
 // The statements in the setup() function
 // execute once when the program begins
 function setup() {
   createCanvas(720, 400); // Size must be the first statement
-  stroke(255); // Set line drawing color to white
-  frameRate(30);
+
+}
+function preload(){
+  //loading all three images
+
+pacman = loadImage ('../imatges/pacman.png');
 }
 // The statements in draw() are executed until the
 // program is stopped. Each statement is executed in
 // sequence and after the last line is read, the first
 // line is executed again.
 function draw() {
-  background(0); // Set the background to black
-  y = y - 1;
-  if (y < 0) {
-    y = height;
-  }
-  line(0, y, width, y);
+
+  background(10,10, 255);
+  image(pacman,mouseX,50,50,50);
+
+
+
+
 }
